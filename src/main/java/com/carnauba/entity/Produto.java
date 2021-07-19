7f78dd0f9846592416100a56add2c6d4c3c27c29
RobertoCarnauba <betocarnauba@hotmail.com>
2021-07-19 17:35:53 -0300

    servico criado

---
+++ src/main/java/com/carnauba/entity/Produto.java
@@ -0,0 +1,29 @@
+package com.carnauba.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "venda")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Produto {
+	@Id
+	private Long id;
+	@Column(name = "estroque", nullable = false, length = 10)
+	private Integer estoque;
+
+}
---
+++ src/main/java/com/carnauba/repository/VendaRepository.java
@@ -0,0 +1,11 @@
+package com.carnauba.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carnauba.entity.Venda;
+
+@Repository
+public interface VendaRepository extends JpaRepository<Venda, Long>{
+
+}
